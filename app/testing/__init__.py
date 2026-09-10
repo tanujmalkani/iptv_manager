222f1dd555fbef44d717b426994b68d1fe4a22b6
tanujmalkani <57258702+tanujmalkani@users.noreply.github.com>
2026-09-10 13:17:13 +0530

    Export deep test interfaces

--- app/testing/__init__.py
+++ app/testing/__init__.py
@@ -1,7 +1,10 @@
+from app.testing.deep import DeepTestEngine, DeepTestRunner
 from app.testing.quick import QuickTestEngine, QuickTestResult, QuickTestRunner
 from app.testing.stream import StreamTestEngine, StreamTestRunner
 
 __all__ = [
+    "DeepTestEngine",
+    "DeepTestRunner",
     "QuickTestEngine",
     "QuickTestResult",
     "QuickTestRunner",
